eb110874a29b16ae3eaf2c8c72ab94db301e01b7
Kelleher Guerin <kguerin.optics@gmail.com>
2013-03-12 23:22:04 +0100

    Cleaned up and removed overshoot code, removed extra ros_param call for pid_gains.

--- velocity_controllers/src/cartesian_setpoint_controller.cpp
+++ velocity_controllers/src/cartesian_setpoint_controller.cpp
@@ -166,14 +166,9 @@ bool CartesianSetpointController::init(hardware_interface::VelocityJointInterfac
   joint_velocities_command_.resize(num_joints_);
   joint_positions_upper_limits_.resize(num_joints_);
   joint_positions_lower_limits_.resize(num_joints_);
-  joint_velocities_overshoot_.resize(num_joints_);
 
   // Create PID Controller
   control_toolbox::Pid pid_controller;
-  if (!pid_controller.init(ros::NodeHandle(node_,"pid_gains"))) return false;
-  for (unsigned int i = 0; i < num_joints_; i++)
-    pid_controller_.push_back(pid_controller);
-
   for (unsigned int i = 0; i < num_joints_; i++){
     if (!pid_controller.init(ros::NodeHandle(node_,"pid_gains/" + chain_joint_names_[i]))){
       ROS_WARN_STREAM("CartesianSetpointController: No PID Gains found on parameter server for joint "<<chain_joint_names_[i]);
@@ -343,29 +338,6 @@ void CartesianSetpointController::update(const ros::Time& time, const ros::Durat
           joint_velocities_command_(i) = joint_velocities_(i) - joint_acceleration_limits_[i];
       }
     }
-
-    // Calculate max overshoot ratio
-    // max_vel_overshoot_ratio_ = 0;
-    // for(unsigned int i=0;i<num_joints_;i++){
-    //   joint_velocities_overshoot_(i) = fabs(joint_velocities_command_(i)) - fabs(joint_velocity_limits_[i]);
-    // }
-    // for(unsigned int i=0;i<num_joints_;i++){
-    //   double overshoot_ratio;
-    //   if(joint_velocities_overshoot_(i) > 0){
-    //     overshoot_ratio = joint_velocities_overshoot_(i) / fabs(joint_velocity_limits_[i]);
-    //     if(overshoot_ratio > max_vel_overshoot_ratio_)
-    //       max_vel_overshoot_ratio_ = overshoot_ratio;
-    //   }  
-    // }
-
-    // // Scale velocities to overshoot ratio
-    // for(unsigned int i=0;i<num_joints_;i++){
-    //   if(max_vel_overshoot_ratio_ > 0){
-    //     joint_velocities_command_(i) = joint_velocities_command_(i)*max_vel_overshoot_ratio_;
-    //   }
-    // }
-
-    // Assign velocity to each joint from command
     for(unsigned int i=0;i<num_joints_;i++){
       // Get Joint Handle
       hardware_interface::JointHandle joint = joint_handles_[i];
